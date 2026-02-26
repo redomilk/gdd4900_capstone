d88445f1fe3c0612e014190a6f5cfb424b00e2ca
redomilk <ldavids3@uccs.edu>
2026-02-26 04:54:46 -0700

    fixed shop text & tilemapped
    - tile mapped background of hub
    - fixed level text

--- Assets/Scripts/UI Scripts/HubShop.cs
+++ Assets/Scripts/UI Scripts/HubShop.cs
@@ -1,53 +1,42 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-
 public class HubShop : MonoBehaviour
 {
     [Header("Scrap Display")]
     public TextMeshProUGUI scrapCountText;
-
     [Header("HP Upgrade")]
     public Image hpProgressBar;
     public TextMeshProUGUI hpLevelText;
     public TextMeshProUGUI hpCostText;
-
     [Header("Oxygen Upgrade")]
     public Image oxygenProgressBar;
     public TextMeshProUGUI oxygenLevelText;
     public TextMeshProUGUI oxygenCostText;
-
     [Header("Speed Upgrade")]
     public Image spdProgressBar;
     public TextMeshProUGUI spdLevelText;
     public TextMeshProUGUI spdCostText;
-
     const int maxLevel = 10;
     const int costPerLevel = 20;
-
     void Start()
     {
         RefreshUI();
     }
-
     void RefreshUI()
     {
         if (GameManager.instance == null) return;
-
         scrapCountText.text = "" + GameManager.instance.scrapCount;
-
-        UpdateUpgradeUI(hpProgressBar, hpLevelText, hpCostText, GameManager.instance.healthLevel);
-        UpdateUpgradeUI(oxygenProgressBar, oxygenLevelText, oxygenCostText, GameManager.instance.oxygenLevel);
-        UpdateUpgradeUI(spdProgressBar, spdLevelText, spdCostText, GameManager.instance.speedLevel);
+        UpdateUpgradeUI(hpProgressBar, hpLevelText, hpCostText, GameManager.instance.healthLevel, "HP");
+        UpdateUpgradeUI(oxygenProgressBar, oxygenLevelText, oxygenCostText, GameManager.instance.oxygenLevel, "Oxygen");
+        UpdateUpgradeUI(spdProgressBar, spdLevelText, spdCostText, GameManager.instance.speedLevel, "Speed");
     }
-
-    void UpdateUpgradeUI(Image bar, TextMeshProUGUI levelText, TextMeshProUGUI costText, int currentLevel)
+    void UpdateUpgradeUI(Image bar, TextMeshProUGUI levelText, TextMeshProUGUI costText, int currentLevel, string upgradeName)
     {
         if (bar != null) bar.fillAmount = (float)currentLevel / maxLevel;
-        if (levelText != null) levelText.text = "Level " + currentLevel + " / " + maxLevel;
+        if (levelText != null) levelText.text = upgradeName + " Level " + currentLevel + " / " + maxLevel;
         if (costText != null) costText.text = currentLevel >= maxLevel ? "MAXED" : "Cost: " + costPerLevel + " scrap";
     }
-
     public void BuyHPUpgrade()
     {
         if (TryPurchase(ref GameManager.instance.healthLevel))
@@ -56,7 +45,6 @@ public class HubShop : MonoBehaviour
             RefreshUI();
         }
     }
-
     public void BuyOxygenUpgrade()
     {
         if (TryPurchase(ref GameManager.instance.oxygenLevel))
@@ -65,7 +53,6 @@ public class HubShop : MonoBehaviour
             RefreshUI();
         }
     }
-
     public void BuySpdUpgrade()
     {
         if (TryPurchase(ref GameManager.instance.speedLevel))
@@ -74,7 +61,6 @@ public class HubShop : MonoBehaviour
             RefreshUI();
         }
     }
-
     bool TryPurchase(ref int level)
     {
         if (level >= maxLevel) { Debug.Log("Already maxed!"); return false; }
